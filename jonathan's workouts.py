fa561ba5fc0c613b7a5c3e3e125a0f1b0423124a
Issac Jacob <67367495+ij264@users.noreply.github.com>
2020-08-06 21:05:27 +0100

    Add files via upload

---
+++ jonathan's workouts.py
@@ -0,0 +1,54 @@
+warmup = ['depth jumping jacks',
+          'high knees running',
+          'plank runs',
+          'jumping jacks']
+
+
+chest = ['single leg drops (both legs raised, lower and return one leg at a time, shoulders off the floor if you can)',
+         'cycles',
+         'triple crunch - knees in table top',
+         'triple crunch legs raised',
+         'Russian twists',
+         'leg lowers',
+         'dish flutters',
+         'crunches',
+         'v sits',
+         ]
+
+glutes = ['bent leg peeing (R)'
+          'bent leg peeing (L)',
+          'on back, hips raised, leg lowers (R)',
+          'on back, hips raised, leg lowers (L)',
+          'on front, straight leg kicks behind (R)',
+          'on front, straight leg kicks behind (L)',
+          'leg pulses on side (R)',
+          'leg pulses on side (L)']
+
+arms = ['elbows - holding plank',
+        'front support - shoulder touches',
+        'regular press ups',
+        'elbows – hip twists',
+        'elbows – Spiderman crawls',
+        'tricep dips',
+        'elbows – opposite arm and leg extensions',
+        'front support – running',
+        'elbows – pulsing left leg behind',
+        'front support – shoulder shrugs',
+        'diamond press ups',
+        'elbows – pulsing right leg behind',
+        'arch – full body flutters',
+        'arch – butterfly']
+
+legs  = ['tuck jumps',
+         'depth jumps',
+         'mountain climbers',
+         'ski jumps',
+         'mcdonalds jumps',
+         'plie jumps',
+         'reverse burpees',
+         'burpees']
+          
+
+finisher = ['press ups',
+            'squat jumps',
+            'leg lowers]
